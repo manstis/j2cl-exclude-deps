2d8964e67a8243b70ef8efd74f7be5ab549b8fc0
Michael Anstis <manstis@redhat.com>
2021-07-19 12:48:50 +0100

    Initial commit

---
+++ src/main/java/org/manstis/j2cl/Pojo.java
@@ -0,0 +1,14 @@
+package org.manstis.j2cl;
+
+public class Pojo {
+
+    private String field1;
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+}
